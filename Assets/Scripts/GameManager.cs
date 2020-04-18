ce21f46a24c9456ecf6a830e2c620461fbcffc3b
Gabriel Santini Quibao <gquibao@gmail.com>
2020-04-18 18:23:09 -0300

    Main mechanics made

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,9 +8,16 @@ public class GameManager : MonoBehaviour
     public static GameManager instance;
     
     public TMP_Text timerText;
+    public TMP_Text finalTimerText;
     private int _time = 0;
     private int _minutes = 0;
     private int _seconds = 0;
+    private int _spawnTime = 15;
+    public GameObject gameOverObject;
+    public GameObject waterCubePrefab;
+    public GameObject magmaCubePrefab;
+    public Transform waterStartingPoint;
+    public Transform magmaStartingPoint;
     private Coroutine _countdownCoroutine;
 
     private void Awake()
@@ -20,29 +27,47 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
+        gameOverObject.SetActive(false);
+        SpawnCubes();
         _countdownCoroutine = StartCoroutine(Countdown());
     }
 
     IEnumerator Countdown()
     {
+        var spawnTimer = 0;
         while (true)
         {
-            ShowTimer();
+            if (spawnTimer == _spawnTime)
+            {
+                SpawnCubes();
+                spawnTimer = 0;
+            }
+
+            spawnTimer++;
             _time++;
+            ShowTimer(timerText);
             yield return new WaitForSeconds(1);
         }
     }
 
-    private void ShowTimer()
+    void SpawnCubes()
+    {
+        Instantiate(magmaCubePrefab, magmaStartingPoint.position, Quaternion.identity);
+        Instantiate(waterCubePrefab, waterStartingPoint.position, Quaternion.identity);
+    }
+
+    private void ShowTimer(TMP_Text text)
     {
         _minutes = _time / 60;
         _seconds = _time - _minutes * 60;
-        timerText.text = $"{_minutes:00}:{_seconds:00}";
+        text.text = $"{_minutes:00}:{_seconds:00}";
     }
 
     public void GameOver()
     {
         Cursor.visible = true;
+        gameOverObject.SetActive(true);
+        ShowTimer(finalTimerText);
         StopCoroutine(_countdownCoroutine);
     }
 }
